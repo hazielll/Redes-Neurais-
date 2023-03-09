902443a0bfca27e2d2ddae485cf7854346e89638
Haziel Sixto Baden Sanchez Hermoza <haziel220065@ilum.cnpem.br>
2023-03-09 17:10:05 -0300

    dei upload nos meus arquivos

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
+def funcao_objetivo(individuo):
+    '''Computa a função objetivo no problema das caixas binárias
+    
+    Argumentos: 
+    individuo: lista contendo os genes das caixas binarias 
+    
+    Retornar: Um valor representando a soma dos genes do individuo
+    '''
+    return sum(individuo)
  
